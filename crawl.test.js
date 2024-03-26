0c6e4e833a3b12a4faabada4b4acaa5219c104dd
MansoorCM <manzoorcm96@gmail.com>
2024-03-26 10:54:06 +0530

    Added function to get urls from html body and one associated test

--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,5 @@
 const { test, expect, describe } = require('@jest/globals')
-const { normalizeURL } = require('./crawler.js')
+const { normalizeURL, getURLsFromHTML } = require('./crawler.js')
 
 describe("normalizeURL", () => {
     
@@ -29,5 +29,20 @@ describe("normalizeURL", () => {
         const actual = normalizeURL(input)
         const expected = "blog.boot.dev/path"
         expect(actual).toEqual(expected);
+    });
+})
+
+describe("getURLSFromHTML", () => {
+    
+    test("getURLSFromHTML", () => {
+        const htmlBody = `<html>
+        <body>
+            <a href="https://blog.boot.dev"><span>Go to Boot.dev</span></a>
+            <a href="https://www.boot.dev/community"><span>Go to Boot.dev community</span></a>
+            <a href="/leaderboard"><span>Go to Boot.dev leaderboards</span></a>
+        </body>
+    </html>`
+        const urls = getURLsFromHTML(htmlBody, "https://blog.boot.dev")
+        expect(urls.length).toEqual(3);
     });
 })
--- crawler.js
+++ crawler.js
@@ -1,4 +1,5 @@
 const url = require('url')
+const { JSDOM } = require('jsdom')
 
 function normalizeURL(inputURL) {
     const parsedURL = url.parse(inputURL, true)
@@ -10,6 +11,29 @@ function normalizeURL(inputURL) {
     return host + pathname
 }
 
+function getURLsFromHTML(htmlBody, baseURL){
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const aElements = dom.window.document.querySelectorAll('a')
+    for (const aElement of aElements){
+      if (aElement.href.slice(0,1) === '/'){
+        try {
+          urls.push(new URL(aElement.href, baseURL).href)
+        } catch (err){
+          console.log(`${err.message}: ${aElement.href}`)
+        }
+      } else {
+        try {
+          urls.push(new URL(aElement.href).href)
+        } catch (err){
+          console.log(`${err.message}: ${aElement.href}`)
+        }
+      }
+    }
+    return urls
+  }
+
 module.exports = {
-    normalizeURL
+    normalizeURL, 
+    getURLsFromHTML
 } 
